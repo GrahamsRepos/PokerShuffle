f0b4eb96d7effd7430267f62c96d1c30f80b07a3
graham <grahamn@agileworks.co.za>
2021-05-06 21:37:24 +0200

    Added live shuffling visual effect

---
+++ src/shuffler/shuffler.ts
@@ -0,0 +1,3 @@
+class cardUtilities {
+  constructor() {}
+}
--- src/tests/testPrintSuite.ts
+++ src/tests/testPrintSuite.ts
@@ -8,14 +8,32 @@ import {
   evaluateDraw,
 } from '../shuffler/functions';
 
-console.log('---Lets Play---\n');
-printDeck([...fullDeck]);
-console.log('---Shuffling Cards---\n');
-const shuffledDeck = shuffleArray(fullDeck);
-printDeck(shuffledDeck);
-console.log('---Drawing Cards---\n');
-const draw = drawCards(shuffledDeck);
-printDraw(draw);
-console.log('\n---Draw Evaluation---\n');
-evaluateDraw(draw);
-console.log('\n');
+const doShuffle = (deck: Array<any>) => {
+  let newDeck = [...deck];
+  let i = 0;
+  while (i < 100) {
+    shuffle(i, newDeck);
+    i++;
+  }
+
+  function shuffle(i: number, newdeck: Array<any>) {
+    setTimeout(function () {
+      console.clear();
+      console.log('---Lets Play---\n');
+      printDeck(deck);
+      console.log('---Shuffling Cards---\n');
+      shuffleArray(newdeck);
+      printDeck(newdeck); // Add tasks to do
+      if (i === 99) {
+        console.log('---Drawing Cards---\n');
+        const draw = drawCards(newDeck);
+        printDraw(draw);
+        console.log('\n---Draw Evaluation---\n');
+        evaluateDraw(draw);
+        console.log('\n');
+      }
+    }, 50 * i);
+  }
+};
+
+doShuffle(fullDeck);
