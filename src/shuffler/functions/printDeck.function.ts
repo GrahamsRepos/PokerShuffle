aba01fe065ff210f2976be79c0a7019158feb1f2
graham <grahamn@agileworks.co.za>
2021-05-07 09:10:14 +0200

    ....

--- src/shuffler/functions/printDeck.function.ts
+++ src/shuffler/functions/printDeck.function.ts
@@ -3,13 +3,9 @@ import { getColoredText } from './getColoredText.function';
 export const printDeck = (deck: card[]) => {
   const deckString = deck.reduce((c, card, i) => {
     if ((i + 1) % 13 === 0 && i !== 0) {
-      return (
-        c + getColoredText(`|${card.cardUnicode}|\n`, card.suiteColor)
-      );
+      return c + getColoredText(`${card.cardUnicode}\n`, card.suiteColor);
     } else {
-      return (
-        c + getColoredText(`|${card.cardUnicode}|`, card.suiteColor)
-      );
+      return c + getColoredText(`${card.cardUnicode} `, card.suiteColor);
     }
   }, '');
   console.log(deckString);
